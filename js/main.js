69898da5a6dbc45ca7913d195ab68897a0d47f54
Kevin Gilboy <kgilboy@imgaineyourweb.com>
2017-08-28 14:37:05 -0500

    navbar complete

--- js/main.js
+++ js/main.js
@@ -11,3 +11,19 @@ $(document).ready(function(){
 
     });
 });
+
+
+$(".navbar-dark ul li a[href^='#']").on('click', function(e) {
+
+   e.preventDefault();
+
+   var hash = this.hash;
+
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 300, function(){
+
+       window.location.hash = hash;
+     });
+
+});
