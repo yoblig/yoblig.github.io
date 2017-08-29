021373d83e4b221b51c17d221e2a3dc51f07401b
Kevin Gilboy <kgilboy@imgaineyourweb.com>
2017-08-28 21:14:01 -0500

    responsive design

--- js/main.js
+++ js/main.js
@@ -1,29 +1,83 @@
 $(document).ready(function(){
-
     $(window).scroll(function(){
-
-        if ($(window).scrollTop() > 970) {
+        if ($(window).scrollTop() > 775) {
             $('.navbar').addClass('navbar-fixed-top');
         }
-        if ($(window).scrollTop() < 971) {
+        if ($(window).scrollTop() < 774) {
             $('.navbar').removeClass('navbar-fixed-top');
         }
-
     });
 });
 
-
-$(".navbar-dark ul li a[href^='#']").on('click', function(e) {
-
+$(".navbar-dark a[href^='#']").on('click', function(e) {
    e.preventDefault();
-
    var hash = this.hash;
-
    $('html, body').animate({
        scrollTop: $(hash).offset().top
      }, 300, function(){
-
        window.location.hash = hash;
      });
+});
+$(".navbar-dark ul li a[href^='#']").on('click', function(e) {
+   e.preventDefault();
+   var hash = this.hash;
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 300, function(){
+       window.location.hash = hash;
+     });
+});
 
+$(".scroll-to-projects a[href^='#']").on('click', function(e) {
+   e.preventDefault();
+   var hash = this.hash;
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 300, function(){
+       window.location.hash = hash;
+     });
+});
+$("footer .scroll-to-top-button a[href^='#']").on('click', function(e) {
+   e.preventDefault();
+   var hash = this.hash;
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 300, function(){
+       window.location.hash = hash;
+     });
+});
+
+$('.js-wp-1').waypoint(function(direction) {
+    $('.js-wp-1').addClass('animated fadeInDown');
+}, {
+    offset: '50%'
+});
+
+$('.js-wp-project').waypoint(function(direction){
+  $('.js-wp-project').addClass('animated fadeInUp');
+}, {
+    offset: '80%'
+});
+
+$('.js-wp-work-right-a').waypoint(function(direction){
+  $('.js-wp-work-right-a').addClass('animated fadeInRight');
+}, {
+    offset: '80%'
+});
+
+$('.js-wp-work-left-a').waypoint(function(direction){
+  $('.js-wp-work-left-a').addClass('animated fadeInLeft');
+}, {
+    offset: '80%'
+});
+$('.js-wp-work-right-b').waypoint(function(direction){
+  $('.js-wp-work-right-b').addClass('animated fadeInRight');
+}, {
+    offset: '80%'
+});
+
+$('.js-wp-work-left-b').waypoint(function(direction){
+  $('.js-wp-work-left-b').addClass('animated fadeInLeft');
+}, {
+    offset: '80%'
 });
